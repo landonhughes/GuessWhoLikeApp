f38157b70b3c6f1027026c41e7339983d2994926
Landon <landonhughesdeveloper@gmail.com>
2023-07-01 20:33:13 -0400

    make progress! Trying to figure out api now. Login is working

--- GuessWhoLikeApp/LoginViewController.swift
+++ GuessWhoLikeApp/LoginViewController.swift
@@ -9,16 +9,41 @@ import UIKit
 import SCSDKLoginKit
 
 class LoginViewController: UIViewController {
-
+    var displayName: String!
+    // Requesting the bitmoji is the only problem!!!
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
         let loginButton = SCSDKLoginButton { success, error in
             guard success == true else { return }
+            print(error.localizedDescription)
         }
+        
+ 
+        
+        
         loginButton.sizeToFit()
         loginButton.center = view.center
         view.addSubview(loginButton)
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let query = SCSDKUserDataQueryBuilder()
+                        .withDisplayName()
+                        .withBitmojiTwoDAvatarUrl()
+                        .build()
+        
+        // Call fetch API
+        SCSDKLoginClient.fetchUserData(with: query) { [weak self] userData, error in
+            guard let data = userData else { return }
+            self?.displayName = data.displayName ?? "Someone"
+            print("Display name:", data.displayName ?? "Someone")
+            print("Avatar url:", data.bitmojiTwoDAvatarUrl ?? "Default url")
+        } failure: { error, _ in
+            guard let error = error else { return }
+            print("Error: \(error.localizedDescription)")
+        }
     }
     
 }
